d3cdd558bd89386eb803850a66216e5da101d7f6
Gohmul <ryanyoubum@gmail.com>
2022-07-07 21:49:11 -0400

    declared global variables

---
+++ game.js
@@ -0,0 +1,17 @@
+var components = {
+  numOfRows: 15,
+  numOfColumns: 15,
+  numOfBombs: 35,
+  bomb: '💣',
+  alive: true,
+  colors: {
+    1: 'blue',
+    2: 'green',
+    3: 'purple',
+    4: 'red',
+    5: 'red',
+    6: 'red',
+    7: 'red',
+    8: 'red'
+  }
+}
