f43eb6c26a8a0ce8ccfdc3aa070ad4be6df1b0d5
Gohmul <ryanyoubum@gmail.com>
2022-07-07 21:50:57 -0400

    table creation function

--- game.js
+++ game.js
@@ -15,3 +15,20 @@ var components = {
     8: 'red'
   }
 }
+
+createTable = () => {
+  var table, row, td, i, j
+  table = document.createElement('table')
+
+  for (i = 0; i < components.numOfRows; i++) {
+    row = document.createElement('tr')
+    for (j = 0; j < components.numOfColumns; j++) {
+      td = document.createElement('td')
+      td.id = cellID(i, j)
+      row.appendChild(td)
+      CellListeners(td, i, j)
+    }
+    table.appendChild(row)
+  }
+  return table
+}
